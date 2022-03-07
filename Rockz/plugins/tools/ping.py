c0a928131fb811ca663dc90eb301223410939b3f
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2022-03-07 20:21:39 +0530

    Add files via upload

--- Rockz/plugins/tools/ping.py
+++ Rockz/plugins/tools/ping.py
@@ -1,1 +1,33 @@
+# @xmartperson
 
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
+from strings import get_command
+from Rockz import app
+from Rockz.utils import bot_sys_stats
+from Rockz.utils.decorators.language import language
+
+### Commands
+PING_COMMAND = get_command("PING_COMMAND")
+
+
+@app.on_message(
+    filters.command(PING_COMMAND) & filters.group & ~BANNED_USERS
+)
+@language
+async def ping_com(client, message: Message, _):
+    start = datetime.now()
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        caption=_["ping_1"],
+    )
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    end = datetime.now()
+    resp = (end - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK)
+    )
