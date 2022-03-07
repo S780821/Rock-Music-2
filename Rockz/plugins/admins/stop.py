600d7e20f80d7840b9e6f8f457fd275c98d72653
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2022-03-07 20:16:24 +0530

    😊

---
+++ Rockz/plugins/admins/stop.py
@@ -0,0 +1,26 @@
+# @xmartperson
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import BANNED_USERS
+from strings import get_command
+from Rockz import app
+from Rockz.core.call import Yukki
+from Rockz.utils.decorators import AdminRightsCheck
+
+# Commands
+STOP_COMMAND = get_command("STOP_COMMAND")
+
+
+@app.on_message(
+    filters.command(STOP_COMMAND) & filters.group & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def stop_music(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return await message.reply_text(_["general_2"])
+    await Yukki.stop_stream(chat_id)
+    await message.reply_text(
+        _["admin_9"].format(message.from_user.mention)
+    )
---
+++ Rockz/plugins/admins/unmute.py
@@ -0,0 +1,30 @@
+# @xmartperson
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import BANNED_USERS
+from strings import get_command
+from Rockz import app
+from Rockz.core.call import Yukki
+from Rockz.utils.database import is_muted, mute_off
+from Rockz.utils.decorators import AdminRightsCheck
+
+# Commands
+UNMUTE_COMMAND = get_command("UNMUTE_COMMAND")
+
+
+@app.on_message(
+    filters.command(UNMUTE_COMMAND) & filters.group & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def unmute_admin(Client, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return await message.reply_text(_["general_2"])
+    if not await is_muted(chat_id):
+        return await message.reply_text(_["admin_7"])
+    await mute_off(chat_id)
+    await Yukki.unmute_stream(chat_id)
+    await message.reply_text(
+        _["admin_8"].format(message.from_user.mention)
+    )
