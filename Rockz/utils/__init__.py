9885927a74c53806850641d7a9321d31802f02bd
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2022-03-07 20:41:56 +0530

    Add files via upload

---
+++ Rockz/utils/__init__.py
@@ -0,0 +1,7 @@
+# @xmartperson
+
+from .database import *
+from .decorators import *
+from .formatters import *
+from .inline import *
+from .sys import *
---
+++ Rockz/utils/exceptions.py
@@ -0,0 +1,5 @@
+# @xmartperson
+
+class AssistantErr(Exception):
+    def __init__(self, errr: str):
+        super().__init__(errr)
