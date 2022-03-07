f864b24513f0576491d87a2a19d6625214766536
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2022-03-07 19:58:17 +0530

    Add files via upload

---
+++ Rockz/core/mongo.py
@@ -0,0 +1,12 @@
+# @xmartperson
+
+from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
+from pymongo import MongoClient
+
+import config
+
+_mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
+_mongo_sync_ = MongoClient(config.MONGO_DB_URI)
+
+mongodb = _mongo_async_.Rock
+pymongodb = _mongo_sync_.Rock
