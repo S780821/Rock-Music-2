be4fc8ea638ed3f363b95f1b0c1b6f7fb711d396
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2022-03-07 20:18:36 +0530

    Add files via upload

--- Rockz/plugins/misc/reload.py
+++ Rockz/plugins/misc/reload.py
@@ -1,1 +1,48 @@
+# @xmartperson
 
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import BANNED_USERS, adminlist
+from strings import get_command
+from Rockz import app
+from Rockz.utils.database import get_authuser_names
+from Rockz.utils.decorators import language
+from Rockz.utils.formatters import alpha_to_int
+
+### Multi-Lang Commands
+RELOAD_COMMAND = get_command("RELOAD_COMMAND")
+
+
+@app.on_message(
+    filters.command(RELOAD_COMMAND) & filters.group & ~BANNED_USERS
+)
+@language
+async def reload_admin_cache(client, message: Message, _):
+    try:
+        chat_id = message.chat.id
+        admins = await app.get_chat_members(
+            chat_id, filter="administrators"
+        )
+        authusers = await get_authuser_names(chat_id)
+        adminlist[chat_id] = []
+        for user in admins:
+            if user.can_manage_voice_chats:
+                adminlist[chat_id].append(user.user.id)
+        for user in authusers:
+            user_id = await alpha_to_int(user)
+            adminlist[chat_id].append(user_id)
+        await message.reply_text(_["admin_20"])
+    except:
+        await message.reply_text(
+            "Failed to reload admincache. Make sure Bot is admin in your chat."
+        )
+
+
+@app.on_callback_query(filters.regex("close") & ~BANNED_USERS)
+async def close_menu(_, CallbackQuery):
+    try:
+        await CallbackQuery.message.delete()
+        await CallbackQuery.answer()
+    except:
+        return
