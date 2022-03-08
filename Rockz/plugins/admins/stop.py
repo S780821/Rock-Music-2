1fbdfeeff2862f8d74490d2388ad94f7e91b0eb0
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2022-03-08 08:34:19 +0530

    Update stop.py

--- Rockz/plugins/admins/stop.py
+++ Rockz/plugins/admins/stop.py
@@ -6,7 +6,7 @@ from pyrogram.types import Message
 from config import BANNED_USERS
 from strings import get_command
 from Rockz import app
-from Rockz.core.call import Yukki
+from Rockz.core.call import Rock
 from Rockz.utils.decorators import AdminRightsCheck
 
 # Commands
@@ -20,7 +20,7 @@ STOP_COMMAND = get_command("STOP_COMMAND")
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
-    await Yukki.stop_stream(chat_id)
+    await Rock.stop_stream(chat_id)
     await message.reply_text(
         _["admin_9"].format(message.from_user.mention)
     )
