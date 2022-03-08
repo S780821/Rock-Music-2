c83fbc47b896d5e2a2116289db1cd6e6af5ce890
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2022-03-08 08:42:35 +0530

    Update playlist.py

--- Rockz/plugins/play/playlist.py
+++ Rockz/plugins/play/playlist.py
@@ -19,7 +19,7 @@ from Rockz.utils.decorators.language import language, languageCB
 from Rockz.utils.inline.playlist import (botplaylist_markup,
                                               get_playlist_markup,
                                               warning_markup)
-from Rockz.utils.pastebin import Yukkibin
+from Rockz.utils.pastebin import Rockbin
 from Rockz.utils.stream.stream import stream
 
 # Command
@@ -45,7 +45,7 @@ async def check_playlist(client, message: Message, _):
         count += 1
         msg += f"\n\n{count}- {title[:70]}\n"
         msg += _["playlist_5"].format(duration)
-    link = await Yukkibin(msg)
+    link = await Rockbin(msg)
     lines = msg.count("\n")
     if lines >= 17:
         car = os.linesep.join(msg.split(os.linesep)[:17])
