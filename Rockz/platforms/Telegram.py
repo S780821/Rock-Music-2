54ab3efc73b3461a9c4db35a1b24fe65cf6eaf22
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2022-03-08 07:17:06 +0530

    Update Telegram.py

--- Rockz/platforms/Telegram.py
+++ Rockz/platforms/Telegram.py
@@ -9,7 +9,7 @@ from pyrogram.types import Voice
 
 import config
 from config import MUSIC_BOT_NAME
-from YukkiMusic import app
+from Rockz import app
 
 from ..utils.formatters import (convert_bytes, get_readable_time,
                                 seconds_to_min)
