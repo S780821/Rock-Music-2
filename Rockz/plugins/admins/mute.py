3695b59407f4ae3aa9e51549ef20871bdead7559
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2022-03-08 08:30:37 +0530

    Update mute.py

--- Rockz/plugins/admins/mute.py
+++ Rockz/plugins/admins/mute.py
@@ -6,7 +6,7 @@ from pyrogram.types import Message
 from config import BANNED_USERS
 from strings import get_command
 from Rockz import app
-from Rockz.core.call import Yukki
+from Rockz.core.call import Rock
 from Rockz.utils.database import is_muted, mute_on
 from Rockz.utils.decorators import AdminRightsCheck
 
@@ -24,7 +24,7 @@ async def mute_admin(cli, message: Message, _, chat_id):
     if await is_muted(chat_id):
         return await message.reply_text(_["admin_5"])
     await mute_on(chat_id)
-    await Yukki.mute_stream(chat_id)
+    await Rock.mute_stream(chat_id)
     await message.reply_text(
         _["admin_6"].format(message.from_user.mention)
     )
