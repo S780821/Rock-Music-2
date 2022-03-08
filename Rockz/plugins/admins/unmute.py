36a5214cd1b9b90c5ea5d51cc53d2e64f705f747
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2022-03-08 08:34:51 +0530

    Update unmute.py

--- Rockz/plugins/admins/unmute.py
+++ Rockz/plugins/admins/unmute.py
@@ -6,7 +6,7 @@ from pyrogram.types import Message
 from config import BANNED_USERS
 from strings import get_command
 from Rockz import app
-from Rockz.core.call import Yukki
+from Rockz.core.call import Rock
 from Rockz.utils.database import is_muted, mute_off
 from Rockz.utils.decorators import AdminRightsCheck
 
@@ -24,7 +24,7 @@ async def unmute_admin(Client, message: Message, _, chat_id):
     if not await is_muted(chat_id):
         return await message.reply_text(_["admin_7"])
     await mute_off(chat_id)
-    await Yukki.unmute_stream(chat_id)
+    await Rock.unmute_stream(chat_id)
     await message.reply_text(
         _["admin_8"].format(message.from_user.mention)
     )
