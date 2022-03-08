e5d32df8852b70bc2926ec9ebd6671430d32ae1c
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2022-03-08 08:31:55 +0530

    Update resume.py

--- Rockz/plugins/admins/resume.py
+++ Rockz/plugins/admins/resume.py
@@ -6,7 +6,7 @@ from pyrogram.types import Message
 from config import BANNED_USERS
 from strings import get_command
 from Rockz import app
-from Rockz.core.call import Yukki
+from Rockz.core.call import Rock
 from Rockz.utils.database import is_music_playing, music_on
 from Rockz.utils.decorators import AdminRightsCheck
 
@@ -24,7 +24,7 @@ async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
-    await Yukki.resume_stream(chat_id)
+    await Rock.resume_stream(chat_id)
     await message.reply_text(
         _["admin_4"].format(message.from_user.mention)
     )
