d65058111049e06d097d7d666206d41c8b072c27
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2022-03-08 06:38:00 +0530

    Update bot.py

--- Rockz/core/bot.py
+++ Rockz/core/bot.py
@@ -13,7 +13,7 @@ class RockBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
-            "RockBot",
+            "Rock-Music-2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
